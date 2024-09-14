dba6f90e64d34d53b73ff386d11a341e0a1a0e8e
Patrick Russell <prussell94@gmail.com>
2024-09-14 00:31:45 -0400

    upped to 0.1.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Euro-2024-simulator',  # name of the module
-    version='0.1.1',
+    name='euro-2024-simulator',  # name of the module
+    version='0.1.2',
     packages=find_packages(),  # automatically finds and includes your packages
     install_requires=[],  # list your dependencies here, if any
 )
