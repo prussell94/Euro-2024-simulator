537bd0723494e92c7668c81462db423c8ee87fda
Patrick Russell <prussell94@gmail.com>
2024-09-14 08:57:30 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='euro-2024-simulator',  # name of the module
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),  # automatically finds and includes your packages
     install_requires=[],  # list your dependencies here, if any
 )
